e5c2bf6799b1c0a308ec54cce787fa9f5f0f9e42
Makis Tracend <makis.tracend@gmail.com>
2015-02-01 01:01:19 -0800

    scaffolding

---
+++ examples/setup.sh
@@ -0,0 +1,15 @@
+
+# Installing the Mental Vein CLI
+
+# 1. Install the main executable
+npm install -g mentalvein-cli
+
+#2. Setup Mental Vein (only needed once)
+mentalvein setup
+
+# you'll be propted with two entries
+# - email: ...
+# - token: ...
+
+# 3. Find out API commands using the help
+mentalvein -h
